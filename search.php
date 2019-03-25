10de19c1e661195182f4fe376a3aff643395d36e
sundios <sundios@gmail.com>
2019-03-25 14:10:31 +0100

    updating stuff

--- search.php
+++ search.php
@@ -21,6 +21,7 @@
 <h1>Search Page</h1>
 <a href="/StreetartDB/">Go back home</a>
 <div class="artwork-container">
+	
 <?php
 	if (isset($_POST['submit-search'])) {
 		$search = mysqli_real_escape_string($link, $_POST['search']);
@@ -28,24 +29,38 @@
 		$result = mysqli_query($link, $sql);
 		$queryResult = mysqli_num_rows($result);
 
-		echo "<h2>There are ".$queryResult." results!</h2>";
+		echo "<div class='results_txt'><h2>There are <span class='badge badge-secondary'>" .$queryResult." </span> results!</h2></div>";
 
 		if($queryResult > 0) {
 			while ($row = mysqli_fetch_assoc($result)) {
-				echo "<div class='artwork-box'>
-				<h3>".$row['a_artist']."</h3>
-				<p>".$row['a_title']."</p>
-				<p>".$row['a_location']."</p>
-				<img src=".$row['a_img'].">
+				echo "
+				<div class='card-deck'>
+				<div class='card'>
+				<img class='card-img-top'src=".$row['a_img'].">
+				<div class='card-body'>
+				<h5 class='card-title'>Title:".$row['a_title']."</h5>
+				<p class='card-text'>Artist:".$row['a_artist']."<p>
+				<p class='card-text'>Location:".$row['a_location']."<p>
 				<a href='artwork.php?title=".$row['a_title']."&location=".$row['a_location']."'>
 				<button type='button' class='btn btn-info'>Info</button></a>
+			</div>
+			</div>
 			</div>";
 			}
 		} else { 
-			echo "There are no results matching your search!";
+			echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+  <strong> There are no results matching your search! </strong> Please try again.
+  <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+  </button>
+</div>";
 		}
 	}
 ?>
-
 	
+
 </div>
+
+
+
+
